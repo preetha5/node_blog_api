bf015f6545e26532d7426febe7980af65ce219d0
preetha <preetha_ramki@hotmail.com>
2018-02-21 22:01:32 -0800

    added mongoose package, fixed bug in blogPost destrcuturing

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 const {PORT, DATABASE_URL} = require('./config');
-const blogPosts = require('./models');
+const {blogPosts} = require('./models');
 const app = express();
 app.use(bodyParser.json());
 
