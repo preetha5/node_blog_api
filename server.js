02381f03a3bccf686e0dbc7325f82c1cb1afcc4f
preetha <preetha_ramki@hotmail.com>
2018-02-21 21:21:42 -0800

    initial commit for mongodb support

--- server.js
+++ server.js
@@ -1,45 +1,140 @@
-
+'use strict';
+const bodyParser = require('body-parser');
 const express = require('express');
-const morgan = require('morgan');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
+const {PORT, DATABASE_URL} = require('./config');
+const blogPosts = require('./models');
 const app = express();
+app.use(bodyParser.json());
 
-const blogRouter = require('./blogRouter');
+//GET requests
+app.get('/posts', (req, res) => {
+  blogPosts.find()
+  .limit(10)
+  .then(posts => {
+    res.json({
+      posts: posts.map((post) => post.serialize())
+    });
+  })
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({message: "Internal server error"});
+  });
+});
 
-// log the http layer
-app.use(morgan('common'));
+//GET request with an ID
+app.get('/posts/:id', (req, res) => {
+  blogPosts.findById(req.params.id)
+  .then(post => post.json(post.serialize()))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({message: "Internal server error"});
+  });
+});
 
-app.use('/blog-posts', blogRouter);
+//POST requests
+app.post('/posts', (req,res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i< requiredFields.length; i++){
+    if (!(field in req.body)){
+      const message = `Error: missing ${field} in req.body`;
+      return res.status(400).json(message);
+    }
+  }
+  blogPosts
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      author: req.body.author,
+      created: req.body.created
+    })
+    .then(post => res.status(201).json(post.serialize()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: "Internal server error"});
+    });
+});
+
+//PUT requests
+app.put('/posts/:id', (req,res) =>{
+  //make sure id in the param and req.body match
+  if(!(req.params.id && req.body.id && req.body.id === req.params.id)){
+    const message = 
+      `Request path id ${req.params.id} and Request body ID 
+      (${req.body.id}) should match`;
+    console.error(message);
+    return res.status(400).json({message: message});
+  }
+
+  const toUpdate = {};
+  const updateFields = ['title', 'content', 'author'];
+  updateFields.forEach(field =>{
+    if (field in req.body){
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  blogPosts.findByIdAndUpdate(req.params.id, { $set: toUpdate})
+  .then(post => res.status(200).json(post.serialize()))
+  .catch(err => {
+    console.error(err);
+    res.status(500).json({message: "Internal server error"});
+  });
+});
+
+//DELETE post
+app.delete('/posts/:id', (req,res) =>{
+  blogPosts
+    .findByIdAndRemove(req.params.id)
+    .then(post => res.status(204).end())
+    .catch(err => res.status(500).json({ message: 'Internal server error' }));
+});
+
+//Catch all for requests
+app.use('*', (req,res) =>{
+  res.status(404).json({message: 'Not Found'});
+})
+
 
 let server;
 
-function runServer(){
-  const port = process.env.PORT || 8080;
+function runServer(databaseURL, port=PORT){
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () =>{
-      console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-      resolve(server);
-    }).on ('error', err => {
-      reject(err);
+    mongoose.connect(databaseURL, {useMongoClient: true}, err => {
+      if(err){
+        reject(err);
+      };
+      server = app.listen(port, ()=>{
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
     });
   });
 }
 
 function closeServer(){
-  return new Promise((resolve, reject) => {
-    console.log("closing server");
-    server.close(err => {
-      if(err){
-        reject(err);
-        return;
-      }
-     resolve();
-    })
-  })
+  return mongoose.disconnect()
+    .then(()=>{
+      return new Promise((resolve, reject) =>{
+        console.log('closing server');
+        server.close(err => {
+          if(err) {
+            return reject(err);
+          }
+          resolve();
+        });
+      });
+    });
 }
 
 if (require.main === module){
-  runServer.catch(err => console.error(err));
+  runServer(DATABASE_URL).catch(err => console.error(err));
 }
 
 module.exports = {app, runServer, closeServer};
